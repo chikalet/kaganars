72e12f4d120ddbb3c8d9f45ad455fc9f52a907a0
chikalet <muhamuha006@mail.ru>
2024-04-18 01:43:32 +0300

    почти last commit

--- routers/user_banner.php
+++ routers/user_banner.php
@@ -7,15 +7,30 @@ function route($method, $urlList, $requestData)
         switch ($urlList[1]) {
             case 'user_banner':
                 $token = substr(getallheaders()['Authorization'], 7);
-                $userFromToken = $link->query("SELECT userID FROM tokens WHERE value='$token'")->fetch_assoc();
-                if (!is_null($userFromToken)) {
-                    $userID = $userFromToken['userID'];
-                    $user = $link->query("SELECT * FROM users WHERE id='$userID'")->fetch_assoc();
-                    echo json_encode($user);
-                } else {
-                    echo "401: Пользователь не авторизован";
+                $tokenSelestResult = $link->query("SELECT * FROM tokens WHERE `value`='$token'")->fetch_assoc();
+                if ((!empty($tokenSelestResult['role'])) && ($tokenSelestResult['role'] == 'admin')){
+                    $userTag = $tokenSelestResult['tag'];
+                    $bannerSelectResult = $link->query("SELECT * FROM `Banner_tags` WHERE `Tag_ID`='$userTag'")->fetch_assoc();
+                    $bannerID = $bannerSelectResult['Banner_ID'];
+                    $bannerDetectedResult = $link->query("SELECT * FROM `Banners` WHERE `Banner_ID`='$bannerID'")->fetch_assoc();
+                    echo "ID баннера:".$bannerDetectedResult['Banner_ID']."  Название баннера:".$bannerDetectedResult['name_banner']."   Фича баннера:".$bannerDetectedResult['Feature_ID']."   Флаг активности:".$bannerDetectedResult['active'];
+                    break;
+                } else if ((!empty($tokenSelestResult['role'])) && ($tokenSelestResult['role'] == 'user')){
+                    $userTag = $tokenSelestResult['tag'];
+                    $bannerSelectResult = $link->query("SELECT * FROM `Banner_tags` WHERE `Tag_ID`='$userTag'")->fetch_assoc();
+                    $bannerID = $bannerSelectResult['Banner_ID'];
+                    $bannerDetectedResult = $link->query("SELECT * FROM `Banners` WHERE `Banner_ID`='$bannerID'")->fetch_assoc();
+                    $bannerActive = $bannerDetectedResult['active'];
+                    if ($bannerActive == 1){
+                        echo "ID баннера:".$bannerDetectedResult['Banner_ID']."  Название баннера:".$bannerDetectedResult['name_banner']."   Фича баннера:".$bannerDetectedResult['Feature_ID']."   Флаг активности:".$bannerDetectedResult['active'];
+                        break;
+                    } else{
+                        echo "баннер по вашему тегу не найден";
+                    }
+            }   else{
+                    var_dump(http_response_code(401));
+                    echo "пользователь не авторизован";
                 }
-                break;
         }
     }
 }
